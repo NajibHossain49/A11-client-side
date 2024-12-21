6629f7da717785996ff96c99a9c99efe1d80fede
NajibHossain49 <najibhossain1234@gmail.com>
2024-12-21 21:03:17 +0600

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout";
+import HomePage from "../pages/HomePage";
+import ErrorPage from "../pages/ErrorPage";
+import Login from "../Authentication/Login";
+import Register from "../Authentication/Register";
+import AllBooks from "../pages/AllBooks";
+import AddBook from "../pages/AddBook";
+import BorrowedBooks from "../pages/BorrowedBooks";
+import PrivateRoute from "./PrivateRoute";
+
+// Create a router object
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/All-Books",
+        element: (
+          <PrivateRoute>
+            <AllBooks />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/Add-Book",
+        element: (
+          <PrivateRoute>
+            <AddBook />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/Borrowed-Books",
+        element: (
+          <PrivateRoute>
+            <BorrowedBooks />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/registration",
+        element: <Register />,
+      },
+    ],
+  },
+  // Catch-all route for unmatched paths
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
+
+export default router;
