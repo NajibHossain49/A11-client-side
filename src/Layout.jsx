d5c1dd0ee8272ceb9af6c95f373e2d0104cfb6f6
NajibHossain49 <najibhossain1234@gmail.com>
2024-12-25 17:15:38 +0600

    Implemented: Dynamic Title for All-routes.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,31 +6,30 @@ import Footer from "./components/Footer";
 const Layout = () => {
   const location = useLocation();
 
-  // Define route-to-title mapping
-  const titles = {
-    "/": "Home - My Library",
-    "/All-Books": "All Books - My Library",
-    "/update-book/:id": "Update Book - My Library",
-    "/Add-Book": "Add Book - My Library",
-    "/Borrowed-Books": "Borrowed Books - My Library",
-    "/book-details/:id": "Book Details - My Library",
-    "/login": "Login - My Library",
-    "/registration": "Register - My Library",
-    // "/categories": "Categories - My Library",
-    // "/categories/:category": "Category Books - My Library",
-    // "*": "404 Error - My Library",
-  };
-
   useEffect(() => {
-    const path = location.pathname;
+    // Define route-to-title mapping
+    const routeTitles = {
+      "/": "Home - Book Library",
+      "/All-Books": "All Books - Book Library",
+      "/Add-Book": "Add a New Book - Book Library",
+      "/Borrowed-Books": "Borrowed Books - Book Library",
+      "/login": "Login - Book Library",
+      "/registration": "Register - Book Library",
+      "/categories": "Categories - Book Library",
+    };
 
-    // Match the route dynamically
-    const dynamicTitle = Object.keys(titles).find((route) =>
-      path.match(new RegExp(`^${route.replace(/:\w+/g, "\\w+")}$`))
-    );
-
-    // Set document title
-    document.title = titles[dynamicTitle] || "My Library";
+    // Dynamic title for routes with parameters
+    if (location.pathname.startsWith("/book-details")) {
+      document.title = "Book Details - Book Library";
+    } else if (location.pathname.startsWith("/categories/")) {
+      const category = location.pathname.split("/").pop();
+      document.title = `${decodeURIComponent(category)} Books - Book Library`;
+    } else if (location.pathname.startsWith("/update-book")) {
+      document.title = "Update Book - Book Library";
+    } else {
+      // Default title or match specific routes
+      document.title = routeTitles[location.pathname] || "Book Library";
+    }
   }, [location]);
 
   return (
